e5793333db536a141e5da0b85327440bef3664d3
jsalgua027 <jsalcedoguarde@gmail.com>
2022-10-21 10:46:11 +0200

    comentarios

--- src/main/java/ejemplos_vico/EjemploFor.java
+++ src/main/java/ejemplos_vico/EjemploFor.java
@@ -15,29 +15,26 @@ public class EjemploFor {
      */
     public static void main(String[] args) {
         // Imprimir del 0 al 10
-        
+
         for (int i = 0; i <= 10; i++) {
-            
+
             System.out.println(i);
-            
-            
+
         }
-        
+
         System.out.println("***************************************");
-        
+
         // del 0 al 20 de dos en dos
-        
-        for (int i = 0; i <=20 ; i+=2) {
-            
+        for (int i = 0; i <= 20; i += 2) {
+
             System.out.println(i);
         }
         System.out.println("*******************************************");
         // de 20 a 0
-        
-        
+
         for (int i = 20; i > 0; i--) {
             System.out.println(i);
         }
     }
-    
+
 }
