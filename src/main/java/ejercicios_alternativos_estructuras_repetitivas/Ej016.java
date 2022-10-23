dcd7ff4dc0dd53571dac854b16f96353de9af299
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-10-23 13:33:46 +0200

    Iniciado Ej016

---
+++ src/main/java/ejercicios_alternativos_estructuras_repetitivas/Ej016.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicios_alternativos_estructuras_repetitivas;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Windows10
+ */
+public class Ej016 {
+
+    /**
+     * @param args the command line arguments
+     * Una cadena de tiendas de autoservicio cuenta con sucursales en C ciudades diferenres del pais
+     * En cada ciudad cuenta con T tiendas y cada tienda cuenta con N empleados, asimismo, cada una 
+     * registra lo que vendio en cada ciudad y cuento recaudó la cadena en un solo día
+     * 
+     * Realiza un algoritmo que permita introducir un día lo que vendio cada empleado de
+     * cada tienda de cada ciudad y saber las ventas totales por tienda, ciudad y totales- 
+     */
+    public static void main(String[] args) {
+        ArrayList<Integer> ciudades=new ArrayList<>();
+        ArrayList<Integer>tiendas=new ArrayList<>();
+        ArrayList<Integer>vendedores=new ArrayList<>();
+        int dia;
+        double vendido;
+        
+        
+        
+    }
+    
+}
