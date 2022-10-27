79fa8f3a873359992ba650013119b19615240768
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2022-10-27 18:18:31 +0200

    Añadido comentarios

--- src/main/java/Ejercicio3E/FabricaDeDulces.java
+++ src/main/java/Ejercicio3E/FabricaDeDulces.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Ejercicio3D;
+package Ejercicio3E;
 
 import java.util.Scanner;
 
@@ -52,9 +52,9 @@ public class FabricaDeDulces {
                          
                                 ***********************La Fabrica De Dulces**************************
                          
-                                        Escriba iniciar para comenzar el programa
+                                        Escriba iniciar para comenzar el programa.
                                         
-                                        Escriba salir para salir del programa
+                                        Escriba salir para salir del programa.
                          
                          
                                 *********************************************************************
@@ -62,11 +62,11 @@ public class FabricaDeDulces {
 
         String textoMenuProductos = """
                             ************************La Esteponera*********************
-                          M1. Para el cálculo con Mantecados de Limón
-                          P1. Para el cálculo con Polvorones
-                          T1. Para el cálculo con Turrón de Chocolate
-                          T2. Para el cálculo con Turrón Clásico
-                          M2. Para el cálculo con Mazapanes
+                          M1. Para el cálculo con Mantecados de Limón.
+                          P1. Para el cálculo con Polvorones.
+                          T1. Para el cálculo con Turrón de Chocolate.
+                          T2. Para el cálculo con Turrón Clásico.
+                          M2. Para el cálculo con Mazapanes.
                          
                          ***************************************************************
                          """;
@@ -95,13 +95,13 @@ public class FabricaDeDulces {
         final String TURRONCLASICO = "Turrón clásico";
         final String MAZAPANES = "Mazapanes";
 
-        do {
+        do {//bucle del menu inicial ---> si escribe salir termina el bucle
             System.out.println(textoMenuPrincipal);
             opcionMenuPrincipal = teclado.nextLine();
 
             if (opcionMenuPrincipal.equalsIgnoreCase("iniciar")) {
 
-                do {
+                do {//bucle menu de productos---> si lo mete erroneo se repite el menu de productos
                     System.out.println(textoMenuProductos);
                     opcionMenuProductos = teclado.nextLine();
                     if ((opcionMenuProductos.equalsIgnoreCase("M1")
@@ -109,39 +109,43 @@ public class FabricaDeDulces {
                             || opcionMenuProductos.equalsIgnoreCase("T1")
                             || opcionMenuProductos.equalsIgnoreCase("T2")
                             || opcionMenuProductos.equalsIgnoreCase("M2"))) {
-                        do {
+                        do { //bucle límite materia prima
 
                             System.out.println("Indique el precio de la Materia Prima");
                             precioMateriaPrimaXUnidad = teclado.nextDouble();
                             // si el precio de la materia prima es correcto
                             if (precioMateriaPrimaXUnidad >= LIMITE_INFERIOR_MATERIA_PRIMA && precioMateriaPrimaXUnidad <= LIMITE_SUPERIOR_MATERIA_PRIMA) {
 
-                                do {
+                                do {//bucle limite de mano de obra
 
                                     System.out.println("indique el precio de la Mano de Obra Por Unidad: ");
                                     manoObraPorUnidad = teclado.nextDouble();
+                                    // si el coste de mano de obra es el correcto
                                     if (manoObraPorUnidad >= LIMITE_INFERIOR_MANO_OBRA && manoObraPorUnidad <= LIMITE_SUPERIOR_MANO_OBRA) {
-
+                                        //para los productos M1 y T1 costes de produccion
                                         if (opcionMenuProductos.equalsIgnoreCase("M1") || opcionMenuProductos.equalsIgnoreCase("T1")) {
 
                                             costeProduccionProductoXUnidad = precioMateriaPrimaXUnidad + manoObraPorUnidad;
+                                                //para M1 precio venta por unidad
                                             if (opcionMenuProductos.equalsIgnoreCase("M1")) {
                                                 precioVentaProductoXUnidad
                                                         = costeProduccionProductoXUnidad + (costeProduccionProductoXUnidad * PORCENTAJE_COSTE_PRODUCION_M1M2P1);
+                                                //para T1 precio venta por unidad
                                             } else {
                                                 precioVentaProductoXUnidad
                                                         = costeProduccionProductoXUnidad + (costeProduccionProductoXUnidad * PORCENATJE_COSTE_PRODUCCION_T1T2);
 
                                             }
-
+                                            // para P1 M2  T2 costes de produccion
                                         } else {
 
                                             costeProduccionProductoXUnidad = precioMateriaPrimaXUnidad + manoObraPorUnidad;
+                                            //para P1 y M2 costes de venta por Unidad
                                             if (opcionMenuProductos.equalsIgnoreCase("P1") || opcionMenuProductos.equalsIgnoreCase("M2")) {
 
                                                 precioVentaProductoXUnidad
                                                         = costeProduccionProductoXUnidad + (costeProduccionProductoXUnidad * PORCENTAJE_COSTE_PRODUCION_M1M2P1);
-
+                                             //para T2 cpstes de venta por Unidad
                                             } else {
                                                 precioVentaProductoXUnidad
                                                         = costeProduccionProductoXUnidad + (costeProduccionProductoXUnidad * PORCENATJE_COSTE_PRODUCCION_T1T2);
@@ -149,7 +153,7 @@ public class FabricaDeDulces {
                                             }
 
                                         }
-
+                                        //beneficios por unidad para todos los productos
                                         beneficioXUnidad = precioVentaProductoXUnidad - costeProduccionProductoXUnidad;
 
                                         cantidadUnidadesParaBeneficio = (int) Math.ceil(BENEFICIO_SUPERAR / beneficioXUnidad);
